7d022c0bf10bc321a243e87d6df1d610e8a3cac7
sudnonk <sudnonk12@yahoo.co.jp>
2018-10-29 17:04:11 +0900

    たくさんのデータを扱えないっぽいので戻す

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"bytes"
-	"database/sql"
 	"fmt"
 	"io"
 	"log"
 	"os/exec"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
@@ -17,51 +14,27 @@ const (
 )
 
 func main() {
-	//DBからCPE一覧を取ってくる
-	db, err := sql.Open("sqlite3", CpeDbPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err := db.Query(
-		`select cpe_uri from categorized_cpes`,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var cpeUri string
-	var num int
-	for rows.Next() {
-		var uri string
-
-		if err := rows.Scan(&uri); err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		cpeUri += uri + "\n"
-		num++
-		if num%1000 == 0 {
-			log.Println(num)
-		}
-	}
-	rows.Close()
-
-	log.Println("DB done.")
-
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 
-	cmd := exec.Command("peco")
+	cmd := exec.Command("sqlite3", CpeDbPath, " 'select cpe_uri from categorized_cpes'")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(1, fmt.Sprint(err)+stderr.String())
+		return
+	}
+
+	cmd = exec.Command("peco")
 	stdin, _ := cmd.StdinPipe()
-	io.WriteString(stdin, cpeUri)
+	io.WriteString(stdin, out.String())
 	stdin.Close()
 
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 
-	log.Println("Run peco.")
 	err2 := cmd.Run()
 	if err2 != nil {
 		log.Fatal(2, fmt.Sprint(err2)+stderr.String())
