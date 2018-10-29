0aa61cee85c4a5d58b12d2bc8fe7325fc0aa30c8
sudnonk <sudnonk12@yahoo.co.jp>
2018-10-29 17:11:57 +0900

    パイプラインにする

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
+	"github.com/mattn/go-pipeline"
 	"log"
-	"os/exec"
 )
 
 const (
@@ -14,31 +12,12 @@ const (
 )
 
 func main() {
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-
-	cmd := exec.Command("sqlite3", CpeDbPath, "-echo", "'select cpe_uri from categorized_cpes'")
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
+	out, err := pipeline.Output(
+		[]string{"sqlite3", CpeDbPath, "'select cpe_uri from categorized_cpes'"},
+		[]string{"peco"},
+	)
 	if err != nil {
-		log.Fatal(1, fmt.Sprint(err)+out.String()+stderr.String())
-		return
+		log.Fatal(out, err)
 	}
-
-	cmd = exec.Command("peco")
-	stdin, _ := cmd.StdinPipe()
-	io.WriteString(stdin, out.String())
-	stdin.Close()
-
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	err2 := cmd.Run()
-	if err2 != nil {
-		log.Fatal(2, fmt.Sprint(err2)+stderr.String())
-	}
-
-	fmt.Println(out)
+	fmt.Println(string(out))
 }
