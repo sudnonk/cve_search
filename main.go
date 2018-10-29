cf8d2fd26c76c738c87a806f711bda3d8a4d9e5a
sudnonk <sudnonk12@yahoo.co.jp>
2018-10-29 16:41:56 +0900

    DBを直接触る

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"fmt"
-	"io"
 	"log"
-	"os/exec"
 )
 
 const (
@@ -14,31 +12,62 @@ const (
 )
 
 func main() {
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-
-	cmd := exec.Command("sqlite3", CpeDbPath, " 'select cpe_uri from categorized_cpes'")
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
+	//DBからCPE一覧を取ってくる
+	db, err := sql.Open("sqlite3", CpeDbPath)
 	if err != nil {
-		log.Fatal(1, fmt.Sprint(err)+stderr.String())
-		return
+		log.Fatal(err)
 	}
 
-	cmd = exec.Command("peco")
-	stdin, _ := cmd.StdinPipe()
-	io.WriteString(stdin, out.String())
-	stdin.Close()
-
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	err2 := cmd.Run()
-	if err2 != nil {
-		log.Fatal(2, fmt.Sprint(err2)+stderr.String())
+	rows, err := db.Query(
+		`select ope_uri from categorized_cpes`,
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println(out)
+	defer rows.Close()
+	var opeUri string
+	for rows.Next() {
+		var uri string
+
+		if err := rows.Scan(&uri); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		opeUri += uri + "\n"
+	}
+
+	fmt.Println(opeUri)
+
+	/*
+
+		var out bytes.Buffer
+		var stderr bytes.Buffer
+
+		cmd := exec.Command("sqlite3 ", CpeDbPath, " 'select cpe_uri from categorized_cpes'")
+		cmd.Stdout = &out
+		cmd.Stderr = &stderr
+
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(1, fmt.Sprint(err)+stderr.String())
+			return
+		}
+
+		cmd = exec.Command("peco")
+		stdin, _ := cmd.StdinPipe()
+		io.WriteString(stdin, out.String())
+		stdin.Close()
+
+		cmd.Stdout = &out
+		cmd.Stderr = &stderr
+
+		err2 := cmd.Run()
+		if err2 != nil {
+			log.Fatal(2, fmt.Sprint(err2)+stderr.String())
+		}
+
+		fmt.Println(out)
+	*/
 }
