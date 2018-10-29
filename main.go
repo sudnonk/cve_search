54455d001fcf36bb7e1c66d2901aaacc9bb338df
sudnonk <sudnonk12@yahoo.co.jp>
2018-10-29 16:03:18 +0900

    コマンドのテスト

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mattn/go-pipeline"
+	"log"
+)
+
+const (
+	CpeDbPath = "/root/cpe.sqlite3"
+	CveDbPath = "/root/cve.sqlite3"
+)
+
+func main() {
+	out, err := pipeline.Output(
+		[]string{"sqlite3", CpeDbPath, "'select cpe_uri from categorized_cpes'"},
+		[]string{"peco"},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(out))
+}
