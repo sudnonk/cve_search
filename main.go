8245a839809ef4a916ef0b1ff64c24c5c93242fb
sudnonk <sudnonk12@yahoo.co.jp>
2018-10-29 19:36:52 +0900

    debug

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ AGAIN:
 	}
 
 	for _, cve := range cves {
+		log.Println(cve.Cvss2Severity)
 		if cve.Cvss2Severity == "HIGH" || cve.Cvss2Severity == "CRITICAL" {
 			fmt.Println("CveID: " + cve.CveID + " Severity: " + cve.Cvss2Severity)
 		}
